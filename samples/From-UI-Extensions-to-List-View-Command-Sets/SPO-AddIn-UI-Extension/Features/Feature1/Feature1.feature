e2956a2ca93177ae7537a4638657b2f283cadc0a
Paolo Pialorsi <paolo@pialorsi.com>
2023-02-05 13:19:16 +0100

    Added SP Add-in UI Extension sample

---
+++ samples/From-UI-Extensions-to-List-View-Command-Sets/SPO-AddIn-UI-Extension/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="4e674358-2618-4453-9db2-35187cc0f97a" description="SharePoint Add-in Feature" featureId="4e674358-2618-4453-9db2-35187cc0f97a" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SPO-Addin-UI-Extension Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="43b6e9ab-03a5-4151-9024-bec37646facd" />
+    <projectItemReference itemId="69a0ab93-f061-4867-b0df-974e9713c64f" />
+    <projectItemReference itemId="4872b842-30b3-406d-a4e8-7b3e46edfbe1" />
+    <projectItemReference itemId="2073e982-5990-4962-a33c-49f712bbac8e" />
+  </projectItems>
+</feature>
---
+++ samples/From-UI-Extensions-to-List-View-Command-Sets/SPO-AddIn-UI-Extension/Scripts/DocumentDetails.js
@@ -0,0 +1,81 @@
+﻿var hostweburl;
+var appweburl;
+var clientContext;
+var hostweb;
+var documentsLibrary;
+var libraryId;
+var itemId;
+
+// This code runs when the DOM is ready and creates a context object which is
+// needed to use the SharePoint object model
+$(document).ready(function () {
+    hostweburl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+    appweburl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+    libraryId = decodeURIComponent(getQueryStringParameter("SPListId"));
+    itemId = decodeURIComponent(getQueryStringParameter("SPListItemId"));
+
+    if (libraryId !== 'undefined' && itemId !== 'undefined') {
+        var scriptbase = hostweburl + "/_layouts/15/";
+        $.getScript(scriptbase + "SP.RequestExecutor.js", execCrossDomainRequest);
+    }
+});
+
+// Make the actual request for the document using the cross-domain Request Executor
+function execCrossDomainRequest() {
+
+    var itemUri = appweburl +
+        "/_api/SP.AppContextSite(@target)/web/lists/GetById('" + libraryId + "')/Items(" + itemId + ")?$select=ID,Title,Created,Modified,ServerRedirectedEmbedUrl&@target='" + hostweburl + "'";
+
+    console.log(itemUri);
+
+    var executor = new SP.RequestExecutor(appweburl);
+
+    // First request, to retrieve the form digest 
+    executor.executeAsync({
+        url: itemUri,
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: function (data) {
+            var jsonObject = JSON.parse(data.body);
+            console.log(jsonObject);
+            var document = jsonObject.d;
+            showDocumentDetails(document);
+        },
+        error: function (data, errorCode, errorMessage) {
+            var errMsg = "Error retrieving the document details: " + errorMessage;
+            $("#error").text(errMsg);
+            $("#error").show();
+        }
+    });
+}
+
+// In case of successful retrieval of the document
+function showDocumentDetails(document) {
+    $("#document").empty();
+
+    if (document !== undefined) {
+
+        var docId = document.ID;
+        var docTitle = document.Title;
+        var docCreated = document.Created;
+        var docModified = document.Modified;
+        var docUrl = document.ServerRedirectedEmbedUrl;
+
+        $("#document").append('<ul>');
+        $("#document").append('<li><a href="' + docUrl + '">[' + docId + '] ' + docTitle + '</a></li>');
+        $("#document").append('<li>Created on: ' + docCreated + '</li>');
+        $("#document").append('<li>Modified on: ' + docModified + '</li>');
+        $("#document").append('</ul>');
+    }
+}
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params =
+        document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    }
+}
